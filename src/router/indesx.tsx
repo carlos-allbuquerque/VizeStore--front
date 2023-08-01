fcf1ce0d014cd3f40d1dac3b96b60c6f7f54ef86
Carlos <carlos@pop-os.localdomain>
2023-08-01 08:23:57 -0300

    feat: create router

---
+++ src/router/indesx.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Routes } from "react-router-dom";
+
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+        <Routes>
+        </Routes>
+    </BrowserRouter>
+  );
+}
